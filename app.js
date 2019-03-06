66f7039eb71d4156eea1048332be3fed08fe9f62
Vidit Mathur <mathur.vidit123@gmail.com>
2019-03-06 19:11:36 +0530

    port updated

--- app.js
+++ app.js
@@ -57,8 +57,9 @@ console.log("I'm here");
 
         if (err) console.log(err);});
 
+var port = process.env.PORT || 5000
 
-var server = app.listen(5000,()=> {
-    console.log('Server is running..on port 5000');
+var server = app.listen(port,()=> {
+    console.log('Server is running..on port ',port);
 
 });
